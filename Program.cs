248aa8540133caa58bbaef55663c4bf0632d6bd5
deshanm123 <deshanm123@gmail.com>
2025-09-11 17:57:52 +0530

    implementing binary search algo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] intArr = { -22, -15, 7, 20, 30, 54 };
+
+Console.WriteLine(BinarySearch(intArr,7)); 
+
+
+int BinarySearch(int[] sorrtedNumberArr, int key)
+{
+    int start = 0;
+    int end = sorrtedNumberArr.Length-1;
+    while (start <= end)
+    {
+        int mid = (start + end )/ 2;
+        if (sorrtedNumberArr[mid] == key)
+        {
+            return mid;
+        }
+        else if (sorrtedNumberArr[mid] > key)
+        {
+            end = mid-1;
+        }
+        else if (sorrtedNumberArr[mid] < key)
+        {
+            start = mid+1 ;
+        }
+    }
+
+    return -1;
+}
