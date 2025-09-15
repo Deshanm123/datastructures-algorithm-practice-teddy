2df7c3664fe7d037b001075bac5456f0cbe6fb99
deshanm123 <deshanm123@gmail.com>
2025-09-15 10:22:29 +0530

    implementing linear serarch algo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[]{ 1, 2, 3, 4, 5, 66, 6, 8, 9, 12 };
+Console.WriteLine(linearSearch(array, 666));
+
+bool linearSearch(int[] array, int key)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == key)
+        {
+            return true;
+        }
+    }
+    return false;
+}
